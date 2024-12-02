a5d20eb223ea6fe85745100ddab782a00b3c8c9d
Gremlin under the bed <chloenibali@gmail.com>
2024-12-02 16:41:10 -0700

    Advent day 1

---
+++ day1/day1.py
@@ -0,0 +1,38 @@
+
+with open('input1.txt', 'r') as file:
+    lines = file.readlines()
+
+leftCol = []
+rightCol = []
+
+for line in lines:
+    numbers = line.split()
+    if len(numbers) == 2:
+        leftCol.append(int(numbers[0]))
+        rightCol.append(int(numbers[1]))
+    else:
+        print("error reading line")
+
+#print(rightCol)
+leftCol.sort()
+rightCol.sort()
+totaldifference = 0
+simScore = 0
+
+if len(leftCol) == len(rightCol):
+    for i in range(len(leftCol)):
+        totaldifference += abs(leftCol[i] - rightCol[i])
+        
+        #similarity score
+        lNum = leftCol[i]
+        rCount = rightCol.count(lNum)
+        simScore += lNum * rCount
+
+else:
+    print("error - columns not equal")
+
+print("Difference = ", totaldifference)
+print("sim", simScore)
+
+
+
