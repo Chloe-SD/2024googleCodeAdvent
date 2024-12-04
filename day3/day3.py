5d8837cc3b63b774971248ab26e5794a0e5370cd
Gremlin under the bed <chloenibali@gmail.com>
2024-12-04 10:06:47 -0700

    cleaed day3 code - addedd instructions for days 1-3

--- day3/day3.py
+++ day3/day3.py
@@ -1,20 +1,15 @@
 import re
 
-def sanitize(input_string):
-    # Split on don't() and keep even parts (0,2,4...)
+def sanitize(input_string): #pt2
+    # Split on don't()
     parts = input_string.split("don't()")
-    result = [parts[0]]
+    result = [parts[0]] # add first part regardless (assume do() at start)
     for part in parts[1:]:
-        #print("part:\n"+part)
-        do_index = part.find('do()')
-        if do_index != -1:
-            #print("found:\n"+part[do_index + 4:])
-            result.append(part[do_index + 4:])
-    return ''.join(result)
+        do_index = part.find('do()') #find first index of do()
+        if do_index != -1: #ignore part if no do() is found
+            result.append(part[do_index + 4:]) #add anying AFTER to first occurance of do() to result
+    return ''.join(result) #return as one big stinr BC that how my part1 code handles it
     
-    
-
-
 def extract_patterns(text):
     regex = r"mul\(\d{1,3},\d{1,3}\)"
     matches = re.findall(regex, text)
@@ -32,7 +27,7 @@ def multiply(item):
 total = 0
 with open('day3\input.txt', 'r') as file:
     content = file.read()
-text = sanitize(content)
+text = sanitize(content) #pt2
 muls = extract_patterns(text)
 for mul in muls:
     #print(mul)
