f3135166f1ecdbf55084f13b358476ce078b3dff
Gremlin under the bed <chloenibali@gmail.com>
2024-12-04 09:51:42 -0700

    day3 part2

--- day3/day3.py
+++ day3/day3.py
@@ -1,10 +1,23 @@
 import re
 
-def extract_patterns():
+def sanitize(input_string):
+    # Split on don't() and keep even parts (0,2,4...)
+    parts = input_string.split("don't()")
+    result = [parts[0]]
+    for part in parts[1:]:
+        #print("part:\n"+part)
+        do_index = part.find('do()')
+        if do_index != -1:
+            #print("found:\n"+part[do_index + 4:])
+            result.append(part[do_index + 4:])
+    return ''.join(result)
+    
+    
+
+
+def extract_patterns(text):
     regex = r"mul\(\d{1,3},\d{1,3}\)"
-    with open('day3\input.txt', 'r') as file:
-        content = file.read()
-        matches = re.findall(regex, content)
+    matches = re.findall(regex, text)
     return matches
 
 def multiply(item):
@@ -17,7 +30,10 @@ def multiply(item):
 
 ##PROGRAM##
 total = 0
-muls = extract_patterns()
+with open('day3\input.txt', 'r') as file:
+    content = file.read()
+text = sanitize(content)
+muls = extract_patterns(text)
 for mul in muls:
     #print(mul)
     temp = multiply(mul)
